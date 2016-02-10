3c1e4ebb911d427b1f64a6dd2bb8981d53abc191
Nick Taylor <taylorn@udel.edu>
2016-02-10 16:49:32 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewarkFoods::Application.config.secret_key_base = '32d1fdf84b1a34557b463eaaef11f045a58e7912da304e23b27f411ae0d447f07e564d35de5eb7743a30424523afc75445da298fb62d86cd77a9e02dbc2d09b8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NewarkFoods::Application.config.session_store :cookie_store, key: '_newark-foods_session'
