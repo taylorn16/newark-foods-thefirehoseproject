d30f5ee14f41d7e45cd15ed7514762a68ce445a6
Nick Taylor <taylorn@udel.edu>
2016-02-12 20:43:26 +0000

    Secured edit, update, and destroy actions for places.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
 
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 2)
@@ -21,19 +21,29 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      flash[:alert] = "You do not have permission to edit places that aren't yours. Not cool."
+      return redirect_to root_path
+    end
   end
 
   def update
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render(:text => "Not Allowed.", :status => :forbidden)
+    end
+
     @place.update_attributes(place_params)
-    redirect_to root_path
+    return redirect_to root_path
   end
 
   def destroy
     @place = Place.find(params[:id])
     @place.destroy
     flash[:notice] = "You have successfully deleted the place called #{@place.name}."
-    redirect_to root_path
+    return redirect_to root_path
   end
 
   private
