4ea7ba1490b8d9c64c458fa6d516d66c777e16be
Nick Taylor <taylorn@udel.edu>
2016-02-12 20:50:21 +0000

    Actually secured destroy action for places.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 2)
@@ -41,6 +41,11 @@ class PlacesController < ApplicationController
 
   def destroy
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render(:text => "Not Allowed.", :status => :forbidden)
+    end
+
     @place.destroy
     flash[:notice] = "You have successfully deleted the place called #{@place.name}."
     return redirect_to root_path
