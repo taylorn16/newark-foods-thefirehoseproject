7fc1f5e77164185670b82f43a1f7bb73ca35320e
Nick Taylor <taylorn@udel.edu>
2016-02-12 21:18:33 +0000

    Added more validations to places.

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ActiveRecord::Base
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, length: {minimum: 5, maximum: 100}
+  validates :address, presence: true, length: {minimum: 10, maximum: 150}
+  validates :description, presence: true, length: {minimum: 10, maximum: 400}
 end
