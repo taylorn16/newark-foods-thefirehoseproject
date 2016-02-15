c0a8a4cd91fee66282401fbd2bd047fc40f8908a
Nick Taylor <taylorn@udel.edu>
2016-02-15 15:56:30 +0000

    Added ratings dropdown for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,16 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
 
+  RATINGS = {
+    "One Star" => "1_star",
+    "Two Stars" => "2_stars",
+    "Three Stars" => "3_stars",
+    "Four Stars" => "4_stars",
+    "Five Stars" => "5_stars"
+  }
+
+  def human_rating
+    RATINGS.invert[self.rating]
+  end
+
 end
