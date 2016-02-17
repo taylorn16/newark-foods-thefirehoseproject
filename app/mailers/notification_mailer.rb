2439a92f8d973d79a972c5e13029872b9c70db4c
Nick Taylor <taylorn@udel.edu>
2016-02-17 14:28:39 +0000

    Initial setup of comment_added mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,3 +1,10 @@
 class NotificationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "no-reply@newark-foods.com"
+
+  def comment_added
+    mail({
+      :to => "taylorn@udel.edu",
+      :subject => "A comment has been added to your place!"
+      })
+  end
 end
