563676e4738516c2c5b985388083c12fe2e6cbe2
Nick Taylor <taylorn@udel.edu>
2016-02-17 21:57:19 +0000

    Secured user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+
+    if @user.id != current_user.id
+      flash[:alert] = "You don't have permission to do that."
+      redirect_to root_path
+    end
+
   end
 
 end
