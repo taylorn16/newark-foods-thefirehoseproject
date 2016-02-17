b4a254f1767f12d5ef3a3e462136b3a690b7938a
Nick Taylor <taylorn@udel.edu>
2016-02-17 20:16:32 +0000

    Created user profile basic page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ NewarkFoods::Application.routes.draw do
     resources :photos, only: :create
   end
 
+  resources :users, only: :show
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
